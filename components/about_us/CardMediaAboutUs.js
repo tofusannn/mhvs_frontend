7ebc66f92570557ce6e3c66dea80df16bd0bd1ab
Mano <peerapat.a@extend-it-resource.com>
2022-09-12 11:03:36 +0700

    landing

---
+++ components/about_us/CardMediaAboutUs.js
@@ -0,0 +1,41 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Link,
+  Typography,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const CardMediaAboutUs = (props) => {
+  const { image, title, link } = props;
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.aboutUsCard}>
+      <CardMedia component="img" image={image} alt="image card"></CardMedia>
+      <CardContent>
+        <Typography mb={3} fontWeight={700} fontSize={18}>
+          {title}
+        </Typography>
+        <Link href="#" sx={{color: "#2DA373", fontWeight: 700, fontSize: 14, textTransform: "uppercase", textDecoration: "none" }}>
+          Share
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CardMediaAboutUs;
+
+const useStyles = makeStyles({
+  aboutUsCard: {
+    height: "100%",
+    maxWidth: 300,
+    maxHeight: 435,
+    boxShadow: "10px 5px 20px #0000001A",
+    borderRadius: 8,
+  },
+});
