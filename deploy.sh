9930e81ae250e53e684e1abbcea752607caa6647
natchaipon <peepee.popoz@gmail.com>
2022-10-06 02:25:15 +0700

    update

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-docker stop con_forntend
-docker rm con_forntend
-docker rmi registry.gitlab.com/sarayut.siriwi/mhvs_frontend
-docker pull registry.gitlab.com/sarayut.siriwi/mhvs_frontend
-docker run -d --name con_forntend -p 3000:3000 registry.gitlab.com/sarayut.siriwi/mhvs_frontend
